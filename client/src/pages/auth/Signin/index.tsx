4c6b164ae70b08e7c91413355a19efec79256967
ZHANG QI <qdzhangqicn@yahoo.co.jp>
2026-02-01 17:57:03 +0900

    1

--- client/src/pages/auth/Signin/index.tsx
+++ client/src/pages/auth/Signin/index.tsx
@@ -1,24 +1,50 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@radix-ui/react-label'
+import { Link } from 'react-router-dom'
+
 export default function Signin() {
   return (
-    <div className="bg-amber-400">
-      <div>
-        <div>
-          <h1>ログイン</h1>
-          <p>GitTixにログイン</p>
+    <div className="flex justify-center font-mono">
+      <div className="space-y-6 p-10 mt-50 rounded-2xl border">
+        <div className="space-y-2">
+          <h1 className="text-2xl">ログイン</h1>
+          <p className="text-sm text-muted-foreground">
+            GitTixにログイン
+          </p>
         </div>
 
-        <form>
+        <form className="space-y-4">
           <div>
-            <span>E-Mail</span>
-            <input type="text" placeholder="you@example.com" />
+            <Label htmlFor="email">E-Mail</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="you@example.com"
+            />
           </div>
 
           <div>
-            <span>Password</span>
-            <input type="text" placeholder="*********" />
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="********"
+            />
           </div>
 
-          <button>ログイン</button>
+          <Button variant="outline" className="w-full">
+            ログイン
+          </Button>
+
+          <Link to="/auth/signup">
+            <Button
+              variant="destructive"
+              className="w-full bg-pink-700"
+            >
+              新規登録
+            </Button>
+          </Link>
         </form>
       </div>
     </div>
