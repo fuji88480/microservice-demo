6e367d5f43d486c9ddb857da032fc4e539941285
ZHANG QI <qdzhangqicn@yahoo.co.jp>
2026-02-01 10:11:34 +0900

    client

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="bg-amber-200">Home</div>
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
